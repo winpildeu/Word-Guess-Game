d24256306a114c506a64cd8ccedfd6c44560f11a
Winfield H <winterfreshness@gmail.com>
2018-11-06 18:03:20 -0500

    Got the instr. to disappear upon first press and set that key to a var

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@ const starterPokemon = ['squirtle', 'bulbasaur', 'charmander'];
 
 // Set default values for the variables
 var guessedLetters = [];
+var computerGuess;
 var win = 0;
 var guess = 10;
 
@@ -13,7 +14,25 @@ var numWinLink = document.getElementById("numWin");
 var remainingGuessLink = document.getElementById("remainingGuess");
 var guessedLettersLink = document.getElementById("guessedLetters");
 
+// Function to randomly choose a Pokemon
+function chooseYou() {
+    computerGuess = starterPokemon[Math.floor(Math.random() * starterPokemon.length)];
+    console.log(computerGuess);
+}
+
 // Function to run when the user presses a letter
 document.onkeyup = function(event) {
 
+    // Determine what key was pressed
+    let keyPress = event.key;
+    // console.log(keyPress);
+
+    // Decrement from the guesses and put the keyPress into the guessedLetters array
+    guess--;
+    guessedLetters.push(keyPress);
+    console.log(guessedLetters);
+    
+
+    // Hide the instructions
+    pressAnyKeyLink.textContent = "---";
 }

65616226185c2d2dbd94f047f69668ac4b48db66
Winfield H <winterfreshness@gmail.com>
2018-11-07 13:47:54 -0500

    Game works minus the win and reset for next game. No styling yet.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,13 @@
-// Array of the pokemon
-const starterPokemon = ['squirtle', 'bulbasaur', 'charmander'];
+// ========== STARTING VARIABLES ==========
 
-// Set default values for the variables
+const starterPokemon = ['squirtle', 'bulbasaur', 'charmander'];
 var word = {
-    "guessedLetters": [],
-    "wordArray": [],
-    "guess": 10,
-    "win": 0
+    guessedLetters: [],
+    wordArray: [],
+    computerGuess: [],
+    guess: 10,
+    win: 0
 }
-var computerGuess;
-// var guessedLetters = [];
-// var wordArray;
-// var win = 0;
-// var guess = 10;
 
 // References to the places in the HTML
 var lettersBlanksLink = document.getElementById("letterBlanks");
@@ -21,19 +16,25 @@ var numWinLink = document.getElementById("numWin");
 var remainingGuessLink = document.getElementById("remainingGuess");
 var guessedLettersLink = document.getElementById("guessedLetters");
 
-// Initialize
-computerGuess = chooseYou(starterPokemon);
-word.wordArray = blankShow(computerGuess.length);
-lettersBlanksLink.textContent = word.wordArray.join(" ");
-numWinLink.textContent = word.win;
-remainingGuessLink.textContent = word.guess;
-guessedLettersLink.textContent = word.guessedLetters;
+// ========== FUNCTIONS ==========
+
+// Reset function
+function reset(obj) {
+    obj.guess = 10;
+    obj.computerGuess = chooseYou(starterPokemon).split('');
+    obj.wordArray = blankShow(obj.computerGuess.length);
+    lettersBlanksLink.textContent = obj.wordArray.join(" ");
+    numWinLink.textContent = obj.win;
+    remainingGuessLink.textContent = obj.guess;
+    guessedLettersLink.textContent = obj.guessedLetters;
+}
 
 // Function to randomly choose a Pokemon and reset the guesses
 function chooseYou(list) {
     return list[Math.floor(Math.random() * list.length)];
 }
 
+// Makes an array that shows blanks instead of letters
 function blankShow(num) {
     let array = [];
     for (let index = 0; index < num; index++) {
@@ -42,7 +43,27 @@ function blankShow(num) {
     return array;
 }
 
-// Function to run when the user presses a letter
+// Function to check for letters matching
+function checkConditions(letter, obj) {
+    let array = [];
+    for (let index = 0; index < obj.computerGuess.length; index++) {
+        if (letter === obj.computerGuess[index]) {
+            array.push(letter);
+        } else if (letter != obj.computerGuess[index] && obj.wordArray[index] === '_') {
+            array.push('_');
+        } else {
+            array.push(obj.computerGuess[index]);
+        }
+    }
+    obj.wordArray = [...array];
+}
+
+// ========== CODE ==========
+
+// Show the initial values on the HTML page
+reset(word);
+
+// Runs when the user presses a letter
 document.onkeyup = function (event) {
 
     // Hide the instructions
@@ -53,16 +74,14 @@ document.onkeyup = function (event) {
 
     // Determine what key was pressed
     let keyPress = event.key;
-    // Check for win/ lose
-    // checkConditions (keyPress, word);
+    // Check for matching letters
+    checkConditions(keyPress, word);
+    //
 
     // Update the letters that were guessed and remaining guesses
     word.guessedLetters.push(keyPress);
+    lettersBlanksLink.textContent = word.wordArray.join(" ");
     guessedLettersLink.textContent = word.guessedLetters;
     remainingGuessLink.textContent = word.guess;
-
-    console.log("Key: " + keyPress);
-    console.log("Remain: " + word.guess);
-    console.log("Guessed letters: " + word.guessedLetters);
 
 }

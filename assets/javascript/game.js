502b7252c2eb09250ee4257c65c2d21d165e0529
Winfield H <winterfreshness@gmail.com>
2018-11-06 23:03:23 -0500

    Chose rand pokemon. Got array blanks to show. Initialize data.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@ const starterPokemon = ['squirtle', 'bulbasaur', 'charmander'];
 
 // Set default values for the variables
 var guessedLetters = [];
+var wordArray;
 var computerGuess;
 var win = 0;
 var guess = 10;
@@ -14,10 +15,26 @@ var numWinLink = document.getElementById("numWin");
 var remainingGuessLink = document.getElementById("remainingGuess");
 var guessedLettersLink = document.getElementById("guessedLetters");
 
-// Function to randomly choose a Pokemon
-function chooseYou() {
-    computerGuess = starterPokemon[Math.floor(Math.random() * starterPokemon.length)];
-    console.log(computerGuess);
+// Initialize
+computerGuess = chooseYou(starterPokemon);
+wordArray = blankShow(computerGuess.length);
+lettersBlanksLink.textContent = wordArray.join(" ");
+numWinLink.textContent = win;
+remainingGuessLink.textContent = guess;
+guessedLettersLink.textContent = guessedLetters;
+
+
+// Function to randomly choose a Pokemon and reset the guesses
+function chooseYou(list) {
+    return list[Math.floor(Math.random() * list.length)];
+}
+
+function blankShow(num) {
+    let array = [];
+    for (let index = 0; index < num; index++) {
+        array.push("_");
+    }
+    return array;
 }
 
 // Function to run when the user presses a letter
@@ -25,13 +42,11 @@ document.onkeyup = function(event) {
 
     // Determine what key was pressed
     let keyPress = event.key;
-    // console.log(keyPress);
+    console.log(keyPress);
 
     // Decrement from the guesses and put the keyPress into the guessedLetters array
     guess--;
     guessedLetters.push(keyPress);
-    console.log(guessedLetters);
-    
 
     // Hide the instructions
     pressAnyKeyLink.textContent = "---";

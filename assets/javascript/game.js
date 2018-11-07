918d82622b9c1a292ed68185d18f7b316f0dd89f
Winfield H <winterfreshness@gmail.com>
2018-11-07 10:45:09 -0500

    Page displays letters guessed/ remaining guesses. Need to input logic.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,17 @@
 const starterPokemon = ['squirtle', 'bulbasaur', 'charmander'];
 
 // Set default values for the variables
-var guessedLetters = [];
-var wordArray;
+var word = {
+    "guessedLetters": [],
+    "wordArray": [],
+    "guess": 10,
+    "win": 0
+}
 var computerGuess;
-var win = 0;
-var guess = 10;
+// var guessedLetters = [];
+// var wordArray;
+// var win = 0;
+// var guess = 10;
 
 // References to the places in the HTML
 var lettersBlanksLink = document.getElementById("letterBlanks");
@@ -17,12 +23,11 @@ var guessedLettersLink = document.getElementById("guessedLetters");
 
 // Initialize
 computerGuess = chooseYou(starterPokemon);
-wordArray = blankShow(computerGuess.length);
-lettersBlanksLink.textContent = wordArray.join(" ");
-numWinLink.textContent = win;
-remainingGuessLink.textContent = guess;
-guessedLettersLink.textContent = guessedLetters;
-
+word.wordArray = blankShow(computerGuess.length);
+lettersBlanksLink.textContent = word.wordArray.join(" ");
+numWinLink.textContent = word.win;
+remainingGuessLink.textContent = word.guess;
+guessedLettersLink.textContent = word.guessedLetters;
 
 // Function to randomly choose a Pokemon and reset the guesses
 function chooseYou(list) {
@@ -38,16 +43,26 @@ function blankShow(num) {
 }
 
 // Function to run when the user presses a letter
-document.onkeyup = function(event) {
-
-    // Determine what key was pressed
-    let keyPress = event.key;
-    console.log(keyPress);
-
-    // Decrement from the guesses and put the keyPress into the guessedLetters array
-    guess--;
-    guessedLetters.push(keyPress);
+document.onkeyup = function (event) {
 
     // Hide the instructions
     pressAnyKeyLink.textContent = "---";
+
+    // Decrement from the guesses
+    word.guess--;
+
+    // Determine what key was pressed
+    let keyPress = event.key;
+    // Check for win/ lose
+    // checkConditions (keyPress, word);
+
+    // Update the letters that were guessed and remaining guesses
+    word.guessedLetters.push(keyPress);
+    guessedLettersLink.textContent = word.guessedLetters;
+    remainingGuessLink.textContent = word.guess;
+
+    console.log("Key: " + keyPress);
+    console.log("Remain: " + word.guess);
+    console.log("Guessed letters: " + word.guessedLetters);
+
 }
